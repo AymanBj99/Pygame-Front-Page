20d81e205015522d31432be8a63a8b655ebabefc
AymanBj99 <ayman.bouajaji66@gmail.com>
2025-02-15 19:33:02 +0100

    update the import of player up and down

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
 right_player= pygame.image.load("C:/Users/ayman/OneDrive/Desktop/kenney_sokoban-pack/PNG/Default_size/Player/player_11.png")
 left_player= pygame.image.load("C:/Users/ayman/OneDrive/Desktop/kenney_sokoban-pack/PNG/Default_size/Player/player_20.png")
+player_up= pygame.image.load("C:/Users/ayman/OneDrive/Desktop/kenney_sokoban-pack/PNG/Default_size/Player/player_07.png")
+player_down= pygame.image.load("C:/Users/ayman/OneDrive/Desktop/kenney_sokoban-pack/PNG/Default_size/Player/player_08.png")
+
 
 
 
@@ -20,6 +23,9 @@ left_player= pygame.image.load("C:/Users/ayman/OneDrive/Desktop/kenney_sokoban-p
 
 right_player= pygame.transform.scale(right_player, (80,80))
 left_player= pygame.transform.scale(left_player, (80,80))
+player_up= pygame.transform.scale(player_up, (80,80))
+player_down= pygame.transform.scale(player_down, (80,80))
+
 
 current_player=right_player
 
@@ -37,8 +43,10 @@ while running:
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
         player_pos.y -= 300 * dt
+        current_player=player_down
     if keys[pygame.K_s]:
         player_pos.y += 300 * dt
+        current_player=player_up
     if keys[pygame.K_a]:
         player_pos.x -= 300 * dt
         current_player=left_player
